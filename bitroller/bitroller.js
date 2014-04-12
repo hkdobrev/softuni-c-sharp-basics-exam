b4aa9cd7ad894b439ddb119e7b1e2b4180bdd9fc
Haralan Dobrev <hkdobrev@gmail.com>
2014-04-12 17:37:43 +0300

    Initial solving of BitRoller

--- bitroller/bitroller.js
+++ bitroller/bitroller.js
@@ -1,7 +1,33 @@
 #!/usr/bin/env node
 
 function Solve (args) {
-    console.log(args);
+    var
+        LIMIT = 19,
+        leftMostBit = 1 << (LIMIT - 1),
+        n = args.shift(),
+        f = args.shift(),
+        r = args.shift(),
+        fixed = n >> f & 1,
+        i = 0,
+        right, previous;
+
+    for ( ; i < r; i++ ) {
+        right = n & 1;
+        previous = (n >> (f + 1)) & 1;
+        n >>= 1;
+        if ( right ) {
+            n = n | leftMostBit;
+        }
+        previous = n >> f & 1;
+        if ( previous ) {
+            n |= (previous << (f - 1));
+        } else {
+            n &= ~(1 << (f - 1));
+        }
+        n |= (fixed << f);
+    }
+
+    return n;
 }
 
 var readline = require('readline'),
